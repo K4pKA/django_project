31e45df7a6a3d94de9c216e3adfd8cf4b784696a
K4pKA <68323544+K4pKA@users.noreply.github.com>
2021-12-25 21:26:14 +0300

    Update settings.py

--- questssite/settings.py
+++ questssite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-(u)wyxqbxm$w=jxd+l%5m&@7r^px36cj^@5hs0)&fsyl@pf3bb'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-(u)wyxqbxm$w=jxd+l%5m&@7r^px36cj^@5hs0)&fsyl@pf3bb')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
